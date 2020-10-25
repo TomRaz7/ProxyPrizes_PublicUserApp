c8887bafc1ba2e3ec2a424f603d4bc3066daa208
TomRaz7 <lecreux.tom@gmail.com>
2020-10-25 17:06:39 +0100

    Map integration

--- App/components/ShopMap.js
+++ App/components/ShopMap.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import MapView from 'react-native-maps';
+
+//Faker to display fake stores
+import faker from '../faker/ShopData';
+
+//Service to geolocate
+import {getLocation} from '../service/CurrentLocationService';
 
 export default class ShopMap extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state ={
+      fakShopList:faker,
+      region:{
+        latitude:42.35,
+        longitude:1.431,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03
+      },
+      userPosition:{
+        latitude:null,
+        longitude:null,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03
+      }
+    }
+  }
+
   render(){
     return(
       <View style={styles.container}>
-        <Text>Bienvenu sur la map</Text>
+        <MapView
+            style={styles.mapView}
+            region={this.state.region}
+            initialRegion={this.state.region}>
+        </MapView>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles =  StyleSheet.create({
+  container:{
+    flex:1
   },
-});
+  mapView: {
+    flex: 1
+  },
+})
---
+++ App/faker/ShopData.js
@@ -0,0 +1,26 @@
+export default faker = [
+  {
+    id:0,
+    name:'Bonpreu',
+    coords:{
+      latitude:42.35,
+      longitude:1.431
+    },
+  },
+  {
+    id:1,
+    name:'Carrefour',
+    coords:{
+      latitude:42.50,
+      longitude:1.433
+    },
+  },
+  {
+    id:2,
+    name:'Suma',
+    coords:{
+      latitude:42.12,
+      longitude:1.411
+    },
+  },
+]
