636135e7bdb61661401741903389db383f5f2314
TomRaz7 <lecreux.tom@gmail.com>
2020-10-26 15:24:18 +0100

    Geolocation settled

--- App/components/ShopMap.js
+++ App/components/ShopMap.js
@@ -21,12 +21,67 @@ export default class ShopMap extends React.Component{
         longitudeDelta: 0.03
       },
       userPosition:{
-        latitude:null,
-        longitude:null,
+        latitude:0,
+        longitude:0,
         latitudeDelta: 0.03,
         longitudeDelta: 0.03
       }
     }
+  }
+
+  componentDidMount(){
+    getLocation().then(data =>{
+      this.updateRegion({
+        latitude:data.latitude,
+        longitude:data.longitude
+      });
+      this.updateUserPosition({
+        latitude:data.latitude,
+        longitude:data.longitude
+      });
+    })
+    navigator.geolocation.watchPosition(
+      position => {
+        this.updateUserPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 0, distanceFilter: 1}
+    )
+  }
+
+  updateUserPosition(location){
+    if(location.latitude != undefined && location.longitude != undefined){
+      this.setState({
+        userPosition:{
+          latitude:location.latitude,
+          longitude:location.longitude
+        }
+      })
+    }
+  }
+
+  updateRegion(location){
+    if(location.latitude != undefined && location.longitude != undefined){
+      this.setState({
+        region:{
+          latitude:location.latitude,
+          longitude:location.longitude,
+          latitudeDelta: 0.3,
+          longitudeDelta: 0.3,
+        }
+      });
+    }
+  }
+
+  renderUserPosition(){
+    return(
+      <MapView.Marker
+      coordinate={this.state.userPosition}>
+      </MapView.Marker>
+    )
   }
 
   render(){
@@ -36,6 +91,7 @@ export default class ShopMap extends React.Component{
             style={styles.mapView}
             region={this.state.region}
             initialRegion={this.state.region}>
+            {this.renderUserPosition()}
         </MapView>
       </View>
     );
