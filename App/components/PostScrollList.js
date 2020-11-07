845ac09f7ad3b7d11ca29903a5e74a043d94cf8d
TomRaz7 <lecreux.tom@gmail.com>
2020-11-07 10:57:40 +0100

    Front integration Posts ScrollList

--- App/components/PostScrollList.js
+++ App/components/PostScrollList.js
@@ -1,11 +1,93 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity,Image,FlatList } from 'react-native';
+import { Icon } from "react-native-elements";
+import faker from '../faker/PostScrollListData';
 
 export default class PostScrollList extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data:faker
+    };
+  }
+
   render(){
     return(
       <View style={styles.container}>
-        <Text>Bienvenu sur la liste de posts</Text>
+        <FlatList
+            style={styles.list}
+            data={this.state.data}
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+            ItemSeparatorComponent={() => {
+              return <View style={styles.separator} />;
+            }}
+            renderItem={(post) => {
+              const item = post.item;
+              return (
+                <View style={styles.card}>
+                  <Image style={styles.cardImage} source={{ uri: item.image }} />
+                  <View style={styles.cardHeader}>
+                    <View>
+                      <View style={styles.timeContainer}>
+                        <Image
+                          style={styles.userImage}
+                          source={{ uri: item.userImage }}
+                        />
+                        <Text style={styles.userTitle}>{item.username}</Text>
+                      </View>
+                      <Text style={styles.title}>{item.title}</Text>
+                      <Text style={styles.description}>{item.description}</Text>
+                      <View style={styles.timeContainer}>
+                        <Icon
+                          name="clock"
+                          type="entypo"
+                          size={15}
+                          style={styles.iconClock}
+                          onPress={() =>
+                            this.props.navigation.navigate("CreatePost")
+                          }
+                        />
+                        <Text style={styles.time}>{item.time}</Text>
+                      </View>
+                    </View>
+                  </View>
+                  <View style={styles.cardFooter}>
+                    <View style={styles.socialBarContainer}>
+                      <View style={styles.socialBarSection}>
+                        <TouchableOpacity style={styles.socialBarButton}>
+                          <Icon name="heart" type="entypo" style={styles.icon} />
+                          <Text style={styles.socialBarLabel}>{item.likes}</Text>
+                        </TouchableOpacity>
+                      </View>
+                      <View style={styles.socialBarSection}>
+                        <TouchableOpacity style={styles.socialBarButton}>
+                          <Icon
+                            name="message"
+                            type="entypo"
+                            style={styles.icon}
+                          />
+                          <Text style={styles.socialBarLabel}>Comment</Text>
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              );
+            }}
+          />
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.touchableOpacityStyle}
+          >
+            <Icon
+              name="circle-with-plus"
+              type="entypo"
+              color="#4A86E8"
+              size={50}
+            />
+          </TouchableOpacity>
       </View>
     );
   }
@@ -14,8 +96,129 @@ export default class PostScrollList extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  list: {
+    paddingHorizontal: 17,
+    backgroundColor: "#E6E6E6",
+  },
+  separator: {
+    marginTop: 10,
+  },
+  /******** card **************/
+  card: {
+    shadowColor: "#00000021",
+    shadowOffset: {
+      width: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    marginVertical: 8,
+    backgroundColor: "white",
+  },
+  cardHeader: {
+    paddingVertical: 17,
+    paddingHorizontal: 16,
+    borderTopLeftRadius: 1,
+    borderTopRightRadius: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  cardContent: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 16,
+  },
+  cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingTop: 12.5,
+    paddingBottom: 12.5,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 1,
+    borderBottomRightRadius: 1,
+    backgroundColor: "#EEEEEE",
+  },
+  cardImage: {
+    flex: 1,
+    height: 150,
+    width: null,
+  },
+  /******** card components **************/
+  title: {
+    fontSize: 18,
+    flex: 1,
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 15,
+    color: "#888",
+    flex: 1,
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  userTitle: {
+    fontSize: 18,
+    flex: 1,
+    marginLeft: 10,
+  },
+  userImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 30 / 2,
+  },
+  time: {
+    fontSize: 13,
+    color: "#808080",
+    marginTop: 5,
+  },
+  iconClock: {
+    width: 15,
+    height: 15,
+    marginRight: 5,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+    marginRight: 5,
+  },
+  iconData: {
+    width: 15,
+    height: 15,
+    marginTop: 2,
+    marginRight: 5,
+  },
+  timeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  /******** social bar ******************/
+  socialBarContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    flex: 1,
+  },
+  socialBarSection: {
+    justifyContent: "center",
+    flexDirection: "row",
+    flex: 1,
+  },
+  socialBarlabel: {
+    marginLeft: 8,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+  },
+  socialBarButton: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  touchableOpacityStyle: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30,
   },
 });
---
+++ App/faker/PostScrollListData.js
@@ -0,0 +1,68 @@
+export default faker = [
+  {
+    id: 1,
+    username: "Tom",
+    userImage: "https://picsum.photos/50",
+    title: "Product 1",
+    time: "37 minutes ago",
+    image: "https://picsum.photos/800/500",
+    likes: 9,
+    description:
+      "This product is so cool! There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. ",
+  },
+  {
+    id: 2,
+    username: "Marcel",
+    userImage: "https://picsum.photos/50",
+    title: "Product 2",
+    time: "2 days ago",
+    image: "https://picsum.photos/800/500",
+    likes: 5,
+    description:
+      "I have already fall in love with this product! There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+  {
+    id: 3,
+    username: "Danillo",
+    userImage: "https://picsum.photos/50",
+    title: "Product 3",
+    time: "2 hours ago",
+    image: "https://picsum.photos/800/500",
+    likes: 76,
+    description:
+      "Amazing building quality and price. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+  {
+    id: 4,
+    username: "Ares",
+    userImage: "https://picsum.photos/50",
+    title: "Product 4",
+    time: "3 hours ago",
+    image: "https://picsum.photos/800/500",
+    likes: 98,
+    description:
+      "In the future, people will be crazy for this! There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+  {
+    id: 5,
+    username: "Francesc",
+    userImage: "https://picsum.photos/50",
+    title: "Product 5",
+    time: "4 hours ago",
+    image: "https://picsum.photos/800/500",
+    likes: 22,
+    description:
+      "Got it free after a sales event! There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+  {
+    id: 6,
+    username: "Claudia",
+    userImage: "https://picsum.photos/50",
+    title: "Product 6",
+    time: "4 hours ago",
+    image: "https://picsum.photos/800/500",
+    likes: 23,
+    description:
+      "This product is soooooo good! There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+  },
+];
