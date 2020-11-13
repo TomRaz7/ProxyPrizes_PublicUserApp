f3215215ce7e7c2e8a07b1e111b1e306c31d2b50
TomRaz7 <lecreux.tom@gmail.com>
2020-11-13 18:48:50 +0100

    Server connection with local Mysql DB

--- App/server/Server.js
+++ App/server/Server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var mysql = require('mysql');
 require('dotenv').config(); //to read env variables defined in the .env
+var http = require('http');
+
 
 //Config
 const port = process.env.SERVER_PORT;
@@ -10,3 +12,19 @@ const server =  express();
 server.listen(port, ()=>{
   console.log(`Server listening on port : ${port}`);
 })
+
+var connection = mysql.createConnection({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+});
+
+connection.connect(function(error){
+  if(error){
+    console.log(error);
+  }
+  else{
+    console.log(`Connected to database : ${process.env.DB_NAME}`);
+  }
+});
