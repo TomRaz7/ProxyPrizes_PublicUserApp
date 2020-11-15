05ecf67232d2798b24d43ec397ca568922df93ab
Danillo Lange <dls5@alumnes.udl.cat>
2020-11-15 19:00:55 +0100

    testing branch

--- App/App.js
+++ App/App.js
@@ -1,19 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import SwitchNavigator from './navigation/SwitchNavigator';
-import {Provider} from 'react-redux';
-import ConfigStore from './storeRedux/ConfigStore';
-import {PersistGate} from 'redux-persist/integration/react';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SwitchNavigator from "./navigation/SwitchNavigator";
+import { Provider } from "react-redux";
+import ConfigStore from "./storeRedux/ConfigStore";
+import { PersistGate } from "redux-persist/integration/react";
+
+// testing branch
 
 export default class App extends React.Component {
-  render(){
+  render() {
     const store = ConfigStore.getStore();
     const persistor = ConfigStore.getPersistor();
     return (
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <SwitchNavigator/>
+          <SwitchNavigator />
         </PersistGate>
       </Provider>
     );
@@ -23,8 +25,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
