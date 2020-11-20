58783d2dad51ea39512891f57566fd78c964feed
TomRaz7 <lecreux.tom@gmail.com>
2020-11-20 11:06:49 +0100

    Front modifications for account creation

--- App/authenticationFlow/CreateAccount.js
+++ App/authenticationFlow/CreateAccount.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Text, View, StyleSheet,TouchableOpacity, TextInput, Image, Modal, TouchableHighlight} from 'react-native';
+import {Text, View, StyleSheet,TouchableOpacity, TextInput, Image, Modal, TouchableHighlight, Button} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import DatePicker from 'react-native-datepicker';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 //Endpoint Config
 import EndpointConfig from '../server/EndpointConfig';
@@ -24,19 +26,27 @@ export default class CreateAccount extends React.Component{
       createAccount:1,
       userMail:'',
       userPassword:'',
+      confirmPassword:'',
       forname:'Terence',
       name:'Chateigné',
       picture:'https://picsum.photos/500',
       adress:'18 rue du Bastion',
-      city:'Pontoise',
+      city:'Lleida',
       birthDate:'1997-09-23',
       phone:'0657893425',
-      isModalVisible:false
+      isModalVisible:false,
+      datePickerVisible:false,
+      date: new Date()
     }
   }
 
   setModalVisible = (visible) => { //The modal will be used to confirm the account creation
     this.setState({ isModalVisible: visible });
+  }
+
+
+  setDatePickerVisible = (visible) => {
+    this.setState({ datePickerVisible: visible });
   }
 
   updateParentState(data){
@@ -49,7 +59,6 @@ export default class CreateAccount extends React.Component{
   }
 
   _createAccount(){
-    console.log("creation du compte");
     fetch(EndpointConfig.fetchCreateAccount,{
       method:'POST',
       body:JSON.stringify({
@@ -92,17 +101,68 @@ export default class CreateAccount extends React.Component{
         <View style={styles.inputContainer}>
           <TextInput style={styles.textInput} placeholder={i18n.t('create_account_mail')} onChangeText={(text) => this.setState({userMail:text})} keyBoardType='email-address' underlineColorAndroid="#4A86E8"/>
           <TextInput style={styles.textInput} placeholder={i18n.t('create_account_passwd')}  onChangeText={(text) => this.setState({userPassword:text})} secureTextEntry={true} underlineColorAndroid="#4A86E8"/>
-          <TextInput style={styles.textInput} placeholder={i18n.t('confirm_passwd')}  onChangeText={(text) => this.setState({userPassword:text})} secureTextEntry={true} underlineColorAndroid="#4A86E8"/>
+          <TextInput style={styles.textInput} placeholder={i18n.t('confirm_passwd')}  onChangeText={(text) => this.setState({confirmPassword:text})} secureTextEntry={true} underlineColorAndroid="#4A86E8"/>
         </View>
-        <LinearGradient style={styles.linearGradient} colors={['#4A86E8','#4A86E8']} start={[0, 1]} end={[1, 0]}>
-          <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._createAccount()}>
-            <Text style={styles.validate}>{i18n.t('postForm.confirm')}</Text>
-          </TouchableOpacity>
-        </LinearGradient>
-        <View style={styles.cancelBtnContainer}>
-          <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._cancel()}>
-            <Text style={{color:'red'}}>{i18n.t('cancel_btn')}</Text>
-          </TouchableOpacity>
+        <View style={styles.personalInformartionContainer}>
+          <View style = {styles.nameFornamePhoneContainer}>
+            <TextInput style={styles.textInput} placeholder={i18n.t('create_account_name')} onChangeText={(text) => this.setState({name:text})} keyBoardType='email-address' underlineColorAndroid="#4A86E8"/>
+            <TextInput style={styles.textInput} placeholder={i18n.t('create_account_forname')} onChangeText={(text) => this.setState({forname:text})} keyBoardType='email-address' underlineColorAndroid="#4A86E8"/>
+            <TextInput style={styles.textInput} placeholder={i18n.t('create_account_phone')} onChangeText={(text) => this.setState({phone:text})} keyBoardType='email-address' underlineColorAndroid="#4A86E8"/>
+          </View>
+          <View style={styles.datePickerContainer}>
+            <DatePicker
+             style={{width: 200}}
+             date={this.state.date}
+             mode="date"
+             placeholder="select date"
+             format="YYYY-MM-DD"
+             confirmBtnText="Confirm"
+             cancelBtnText="Cancel"
+             customStyles={{
+               dateIcon: {
+                 position: 'absolute',
+                 left: 0,
+                 top: 4,
+                 marginLeft: 0
+               },
+               dateInput: {
+                 marginLeft: 36
+               }
+             }}
+             onDateChange={(date) => {this.setState({date: date})}}
+           />
+         </View>
+         <View style={styles.adressContainer}>
+           <TextInput style={styles.textInput} placeholder={i18n.t('create_account_adress')} onChangeText={(text) => this.setState({adress:text})} keyBoardType='email-address' underlineColorAndroid="#4A86E8"/>
+           <DropDownPicker
+             items={[
+                 {label: 'Lleida', value: 'Lleida'},
+                 {label: 'Sabadell', value: 'Sabadell'},
+             ]}
+             defaultValue={this.state.city}
+             containerStyle={{height: 40, width:150}}
+             style={{backgroundColor: '#4A86E8'}}
+             itemStyle={{
+                 justifyContent: 'flex-start',
+             }}
+             dropDownStyle={{backgroundColor: '#fafafa'}}
+             onChangeItem={item => this.setState({
+                 city: item.value
+             })}
+          />
+         </View>
+        </View>
+        <View style={styles.btnContainer}>
+          <LinearGradient style={styles.linearGradient} colors={['#4A86E8','#4A86E8']} start={[0, 1]} end={[1, 0]}>
+            <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._createAccount()}>
+              <Text style={styles.validate}>{i18n.t('postForm.confirm')}</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+          <View style={styles.cancelBtnContainer}>
+            <TouchableOpacity style={styles.touchableOpacity} onPress={() => this._cancel()}>
+              <Text style={{color:'red'}}>{i18n.t('cancel_btn')}</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <View style={styles.centeredView}>
           <Modal
@@ -113,22 +173,22 @@ export default class CreateAccount extends React.Component{
                 Alert.alert("Modal has been closed.");
               }}
             >
-              <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                  <Text style={styles.modalText}>{i18n.t('account_created_confirmation')} {this.state.userMail}</Text>
-                  <TouchableHighlight
-                    style={{ ...styles.openButton, backgroundColor: "#2196F3", margin:20 }}
-                    onPress={() => {
-                      this.setModalVisible(false);
-                      this._cancel();
-                    }}
-                  >
-                    <Text style={styles.textStyle}>{i18n.t('postForm.confirm')}</Text>
-                  </TouchableHighlight>
-                </View>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>{i18n.t('account_created_confirmation')} {this.state.userMail}</Text>
+                <TouchableHighlight
+                  style={{ ...styles.openButton, backgroundColor: "#2196F3", margin:20 }}
+                  onPress={() => {
+                    this.setModalVisible(false);
+                    this._cancel();
+                  }}
+                >
+                  <Text style={styles.textStyle}>{i18n.t('postForm.confirm')}</Text>
+                </TouchableHighlight>
               </View>
-            </Modal>
-          </View>
+            </View>
+          </Modal>
+        </View>
       </View>
     )
   }
@@ -141,6 +201,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btnContainer:{
+    top:30,
+    justifyContent:'center',
+    alignItems:'center'
+  },
   imageContainer:{
     margin:10
   },
@@ -152,6 +217,27 @@ const styles = StyleSheet.create({
     justifyContent:'space-around',
     height:100,
     width:250,
+  },
+  personalInformartionContainer:{
+    margin:5,
+    height:200,
+    width:250,
+    marginBottom:10
+  },
+  datePickerContainer:{
+    marginLeft:15,
+    marginTop:15
+  },
+  adressContainer:{
+    marginTop:25,
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center'
+  },
+  nameFornamePhoneContainer:{
+    flexDirection:'row',
+    justifyContent:'space-around',
+    alignItems:'center'
   },
   textInput:{
     height:40,
@@ -208,7 +294,6 @@ const styles = StyleSheet.create({
   modalText: {
     marginBottom: 15,
     textAlign: "center",
-    color: "white",
     fontWeight: "bold",
   },
   openButton: {
